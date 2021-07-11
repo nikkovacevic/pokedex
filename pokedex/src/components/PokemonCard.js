9663734367de60823c96ac595146adaaf2ca0214
Nik Kovačević <nik.kovacevic@student.um.si>
2021-07-11 12:44:01 +0200

    type colors gotovi

--- pokedex/src/components/PokemonCard.js
+++ pokedex/src/components/PokemonCard.js
@@ -1,20 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {Link} from 'react-router-dom'
+import typeColors from '../assets/pokemonTypes';
 
 
 export default function PokemonCard(props) {
 
+    
+
+    const [types, setTypes] = useState([]);
+    console.log(props);
+
+    const getPokemon = () => {
+        return fetch(`https://pokeapi.co/api/v2/pokemon/${props.id}`).then((response) => response.json());
+    }
+
+    
+    useEffect(() => {
+        getPokemon().then((data) => {
+            setTypes(data.types.map(type => type.type.name));
+        })
+    }, []);
+    
+    console.log(types)
+
     return (
         <>
             <Link to={`/pokemon/${props.id}`}> 
-        <div className="pokeboxwrapper">
+        <div className="pokeboxwrapper" style={{ backgroundImage: `linear-gradient(to right, ${typeColors[types[0]]}, ${typeColors[types[types.length-1]]})`}}>
             
             <img 
                 className="pokeimg"
                 src={`https://pokeres.bastionbot.org/images/pokemon/${props.id}.png`}
                 alt={`${props.name}`}
             /> 
-            
             
              <p className="pokename">
                  {props.name.toUpperCase()}
